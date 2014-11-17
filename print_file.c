776276a45df59081bbdcb807effaad86ffc19492
ntarradeESGI <nico.tarrade@free.fr>
2014-11-17 23:06:36 +0100

    Fusion
    Creation des fonctions de fusions de div

--- print_file.c
+++ print_file.c
@@ -5,33 +5,33 @@
 int main(int argc,char** argv){
 	if (1 > argc) {
 		printf("Nombre de parametres insuffisants\n");
-		return 1; 
+		return 1;
 	}
 	FILE *f_in;
-	int nb_ligne = 1; 
+	int nb_ligne = 1;
 	int ligne = 0;
 	int i = 0;
 	int j = 0;
 	int c;
 	short entre_accolade = 0;
 	int nb_div = 0;
-	
-	
+
+
 	if (NULL == (f_in = fopen(argv[1],"r"))) {
 		fprintf(stderr, "\nErreur: Impossible de lire %s\n",argv[1]);
-		return(EXIT_FAILURE); 
+		return(EXIT_FAILURE);
 	}
 
 	while ((c = fgetc(f_in)) != EOF){
 		if('\n' == c){
 			nb_ligne++;
-		}	
+		}
 	}
 	 fclose(f_in);
   	char texte[nb_ligne][200];
 	if (NULL == (f_in = fopen(argv[1],"r"))) {
 		fprintf(stderr, "\nErreur: Impossible de lire %s\n",argv[1]);
-		return(EXIT_FAILURE); 
+		return(EXIT_FAILURE);
 	}
 		while ((c = fgetc(f_in)) != EOF){
 			if('\n' == c){
@@ -54,11 +54,12 @@ for(ligne = 0 ; ligne < nb_ligne ; ligne ++){
 				div[nb_div][k] = texte[ligne - 1][k];
 				k++;
 			}
-			
+
 		}
-		
+
 		i++;
 	}
+
 /*
 	for(j = 0 ; j < i - 1 ;j ++){
 		if(texte[ligne][j] == 123){
@@ -72,11 +73,27 @@ for(ligne = 0 ; ligne < nb_ligne ; ligne ++){
 			entre_accolade = 0;
 		}
 	}*/
-}	
-	
+}
+int x;
+for( x = 0 ; x < nb_ligne ; x++)
+{
+    i = 0;
+	while(texte[ligne][i] != 0){
+		if(texte[ligne][i] == 123){
+			int k = 0;
+			while(texte[ligne - 1][k] != '\n'){
+				div[nb_div][k] = texte[ligne - 1][k];
+				k++;
+			}
+
+		}
+
+		i++;
+	}
+}
+
   // Fermeture des flots de donnees
   fclose(f_in);
 
   return(EXIT_SUCCESS);
 }
-                                                                                                   
---
+++ test_div.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char** test_div (char** div1, char** div2, int length_d1, int length_d2)
+{
+    int i;
+    int j;
+    short bol = 0;
+    r
+    for ( i = 0 ; i < length_d1 , i++)
+    {
+        for ( j = 0 ; j < length_d2, j++ )
+        {
+            if (div1[i] != div2[j])
+            {
+                bol = 1;
+            }
+        }
+    }
+    if ( 0 == bol ) {
+        char result[length_d1 + legnth_d2][200]reunite(div, div2);
+        return result;
+    }
+    else
+    {
+        return NULL;
+    }
+}
+
+char** reunite (char** tab1, char** tab2, char* title1, char* title2,int lengtht1, int lengtht2, int length1, int length2)
+{
+    char result[length1 + length2][200];
+    int i, j, k;
+    for ( i = 0 ; i <  length1 + length2 ; i++)
+    {
+        for ( j = 0 ; j < lengtht1 + )
+    }
+
+}
