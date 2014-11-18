70579152eb0c24e400cb9e9e7c7feeba85515243
ntarradeESGI <nico.tarrade@free.fr>
2014-11-18 22:31:49 +0100

    Remplissage du ficier

--- print_file.c
+++ print_file.c
@@ -5,65 +5,51 @@
 int main(int argc,char** argv){
 	if (1 > argc) {
 		printf("Nombre de parametres insuffisants\n");
-		return 1; 
+		return 1;
 	}
 	FILE *f_in;
-	int nb_ligne = 1; 
+	FILE *random;
+	random = fopen("random.css","w");
+	int nb_ligne = 1;
 	int ligne = 0;
 	int i = 0;
-	int j = 0;
 	int c;
-	short entre_accolade = 0;
-	int nb_div = 0;
-	
-	
 	if (NULL == (f_in = fopen(argv[1],"r"))) {
 		fprintf(stderr, "\nErreur: Impossible de lire %s\n",argv[1]);
-		return(EXIT_FAILURE); 
+		return(EXIT_FAILURE);
 	}
 
 	while ((c = fgetc(f_in)) != EOF){
 		if('\n' == c){
 			nb_ligne++;
-		}	
+		}
 	}
 	 fclose(f_in);
+    char textec[nb_ligne][200];
   	char texte[nb_ligne][200];
 	if (NULL == (f_in = fopen(argv[1],"r"))) {
 		fprintf(stderr, "\nErreur: Impossible de lire %s\n",argv[1]);
-		return(EXIT_FAILURE); 
+		return(EXIT_FAILURE);
 	}
 		while ((c = fgetc(f_in)) != EOF){
 			if('\n' == c){
 				texte[ligne][i] = '\0';
+				textec[ligne][i] = texte[ligne][i];
 				ligne++;
 				i = 0;
 			}
 			else{
 				texte[ligne][i] = c;
 			}
-			
 			i++;
 		}
-		
-char div[nb_ligne][200];
-for(ligne = 0 ; ligne < nb_ligne ; ligne ++){
-	i = 0;
-	while(texte[ligne][i] != 0){
-		if(125 == texte[ligne][i]){		
-		div[nb_div][i] = '\0';	
-			nb_div++;
-			printf("\n");
-		}		
-		div[nb_div][i] = texte[ligne][i];
-		i++;
-	}
 
-}	
-	
+for(i = 0; i < nb_ligne ; i++){
+        fputs(textec[i] ,random);
+        printf(" %s \n",textec[i] );
+}
   // Fermeture des flots de donnees
   fclose(f_in);
 
   return(EXIT_SUCCESS);
-}
-                                                                                                   
+	}
